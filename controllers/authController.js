0b6d56401be6383701ccf04d87d5a6df2c74d05f
ahmettKaya <miracky92@gmail.com>
2023-01-17 00:31:00 +0300

    authController refactored to MongoDB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,11 @@
-const fsPromises = require('fs').promises
-const path = require('path')
+const User = require('../model/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
 
 const handleLogin = async (req, res) => {
     const { name, password } = req.body
     if(!name || !password) return res.status(400).json({"message": "Username and password required!"})
-    const foundUser = usersDB.users.find(user => user.name === name)
+    const foundUser = await User.findOne({name: name}).exec()
     if(!foundUser) return res.sendStatus(401)
     const match = await bcrypt.compare(password, foundUser.password)
     if (match) {
@@ -32,13 +26,8 @@ const handleLogin = async (req, res) => {
             process.env.REFRESH_TOKEN_SECRET,
             {expiresIn: '1d'}
         )
-        const otherUsers = usersDB.users.filter(user => user.name !== name)
-        const currenUser = {...foundUser, refreshToken}
-        usersDB.setUsers([...otherUsers, currenUser])
-        await fsPromises.writeFile(
-            path.join(__dirname, "..", "model", "users.json"), 
-            JSON.stringify(usersDB.users)
-        )
+        const currentUser = {...foundUser, refreshToken}
+        await User.updateOne({name: name}, {refreshToken: refreshToken})
         res.cookie('jwt', refreshToken, {httpOnly: true, sameSite: 'None', secure: true, maxAge: 24*60*60*1000})
         res.json({accesToken})
     } else {
